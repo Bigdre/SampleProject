3ec4bc9c6737d86e49b9c5c5507cad1c7f6606cc
Andre Pierce <apierce@walser.com>
2023-04-27 21:51:07 -0400

    Added a SvelteKit Typescript project.

---
+++ Svelte/sveltekit-ts/src/lib/Test.svelte
@@ -0,0 +1,3 @@
+<div>
+    <p>Test Svelte Component</p>
+</div>
---
+++ Svelte/sveltekit-ts/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Test from "$lib/Test.svelte";
+</script>
+
+<main>
+    <Test />
+    <slot></slot>
+</main>
+
+<style>
+    main {
+        display: flex;
+    }
+</style>

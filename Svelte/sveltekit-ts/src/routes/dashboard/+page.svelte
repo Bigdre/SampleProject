22f8dbb03071b2a688d683bf616e39851379bc1f
Andre Pierce <apierce@walser.com>
2023-05-04 21:04:46 -0400

    Added BaseButton to the dashboard

---
+++ Svelte/sveltekit-ts/src/routes/dashboard/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import {
+		type IBaseButtonProps,
+		ButtonVariants,
+		ButtonSizes,
+		ButtonTypes
+	} from '$lib/components/common/components/BaseButton';
+	import BaseButton from '$lib/components/common/components/BaseButton.svelte';
+
+	const props: IBaseButtonProps = {
+		variant: ButtonVariants.Primary,
+		size: ButtonSizes.Medium,
+		type: ButtonTypes.Button,
+		isOutline: false,
+		isDisabled: false,
+		toggle: undefined,
+		customCss: undefined,
+		href: undefined
+	};
+</script>
+
+<BaseButton {props}>Test</BaseButton>
+<BaseButton props={{ ...props, type: ButtonTypes.Input, isOutline: true }} />
